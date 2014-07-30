e04e6408c26aba798e99205741c410fc27376bdc
David <dwrober@gmail.com>
2014-07-29 22:04:31 -0600

    count down timer added

--- src/com/example/pomodoro/PomodoroFragment.java
+++ src/com/example/pomodoro/PomodoroFragment.java
@@ -1,11 +1,8 @@
 package com.example.pomodoro;
 
-import android.app.AlertDialog;
 import android.app.Fragment;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +12,7 @@ import android.widget.TextView;
 public class PomodoroFragment extends Fragment {
 	public String taskName = "";
 	long startTime = 0;
+	private CountDownTimer cdt;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,    		
@@ -39,24 +37,41 @@ public class PomodoroFragment extends Fragment {
 	        public void onClick(View v) {
 	            Button b = (Button) v;
 	            if (b.getText().equals("stop")) {
-	                timerHandler.removeCallbacks(timerRunnable);
+	                stopTimer();
 	                b.setText("start");
 	            } else {
-	                startTime = System.currentTimeMillis();
-	                timerHandler.postDelayed(timerRunnable, 0);
+	                startTimer();
 	                b.setText("stop");
 	            }
 	        }
 	    });
 	}
 	
-    @Override
-    public void onPause() {
-        super.onPause();
-        timerHandler.removeCallbacks(timerRunnable);
-        Button b = (Button)getActivity().findViewById(R.id.start_btn);
-        b.setText("start");
-    }
+	private void stopTimer() {
+		cdt.cancel();
+	}
+	
+	private void startTimer() {
+		//int duration = 25*60*1000;
+		int duration = 10000;
+		TextView timerText = (TextView) getActivity().findViewById(R.id.timer_text);
+		cdt = new CountDownTimer(duration, 1000) {
+			
+		public void onTick(long millisUntilFinished) {
+		    int seconds = (int)millisUntilFinished/1000;
+		  	int minutes = seconds / 60;
+		  	seconds = seconds % 60;
+
+	  		TextView timerText = (TextView) getActivity().findViewById(R.id.timer_text);
+	  		timerText.setText(String.format("%d:%02d", minutes, seconds));
+		}
+
+            public void onFinish() {
+            	TextView timerText = (TextView) getActivity().findViewById(R.id.timer_text);
+            	timerText.setText("done!");
+            }
+         }.start();	
+	}
 	
 	private void setTask() {
 		TextView tv = (TextView) getActivity().findViewById(R.id.task_text);
@@ -64,21 +79,4 @@ public class PomodoroFragment extends Fragment {
 		tv.setText("Task: " + taskName);
 		timerText.setText("25:00");
 	}
-	
-	Handler timerHandler = new Handler();
-    Runnable timerRunnable = new Runnable() {
-        @Override
-        public void run() {
-            long millis = System.currentTimeMillis() - startTime;
-            int seconds = (int) (millis / 1000);
-            int minutes = seconds / 60;
-            seconds = seconds % 60;
-
-            TextView timerText = (TextView) getActivity().findViewById(R.id.timer_text);
-            timerText.setText(String.format("%d:%02d", minutes, seconds));
-
-            timerHandler.postDelayed(this, 500);
-        }
-    };
-
 }
